710d49bbaa88fe565372a08850284bfacd9503e6
TheKrystof701 <krystofcejchan@seznam.cz>
2023-11-27 00:01:58 +0100

    New Crop added ; old deleted

--- phone_client/lib/image_proccessing/colour_picking/wall_colour_picker.dart
+++ phone_client/lib/image_proccessing/colour_picking/wall_colour_picker.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../../helpers/custom_image_class.dart' as custom;
-import '../new_img_crop.dart';
+import '../image_cropping.dart';
 import 'route_and_wall_global_constants.dart';
 
 class ColorPickerWidget extends StatefulWidget {
