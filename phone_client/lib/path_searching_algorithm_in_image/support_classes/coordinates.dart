24e200f8f426f2b966022c66e733b1f274225987
Kryštof Čejchan <krystofcejchan@seznam.cz>
2024-01-02 15:18:23 +0100

    New Test Added for grid generation

--- phone_client/lib/path_searching_algorithm_in_image/support_classes/coordinates.dart
+++ phone_client/lib/path_searching_algorithm_in_image/support_classes/coordinates.dart
@@ -6,6 +6,11 @@ class Coordinates extends Coordinate {
   final int startX, startY, endX, endY;
   Coordinates(this.startX, this.startY, this.endX, this.endY)
       : super(startX, startY);
+
+  factory Coordinates.fromList(List<int> list) {
+    if (list.length < 4) throw Exception('List length not satisfiable');
+    return Coordinates(list.first, list[1], list[2], list[3]);
+  }
   factory Coordinates.recalculate(int x, int y, int x2, int y2) {
     var localFingerOffset = CrossPainter.fingerOffset;
     return Coordinates(x - localFingerOffset, y - localFingerOffset,
--- phone_client/test/path_algorithm_test.dart
+++ phone_client/test/path_algorithm_test.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:phone_client/path_searching_algorithm_in_image/search_maze_algorithms/search_for_shortest_path_in_array.dart';
 import 'package:phone_client/path_searching_algorithm_in_image/support_classes/coordinates.dart';
 import 'package:test/test.dart';
@@ -24,6 +27,32 @@ void main() {
           PathInMatrix pim = PathInMatrix(testGrid, Coordinates(0, 40, 44, 10));
 
           expect(pim.foundPath.isEmpty, true);
+        },
+      );
+      test(
+        'Randomly generated grid',
+        () {
+          final v = Random().nextInt(10000);
+          List<int> randomCoordinates = List.generate(
+            4,
+            (_) => Random().nextInt(v),
+            growable: false,
+          );
+
+          final testGrid = List<List<int>>.filled(
+              v, List<int>.filled(v, Random().nextInt(2)));
+
+          testGrid[randomCoordinates[0]][randomCoordinates[1]] = 0;
+          testGrid[randomCoordinates[2]][randomCoordinates[3]] = 0;
+
+          if (kDebugMode) {
+            print(randomCoordinates.toString());
+          }
+
+          PathInMatrix pim =
+              PathInMatrix(testGrid, Coordinates.fromList(randomCoordinates));
+
+          expect(pim.foundPath.isNotEmpty, pim.foundPath.isNotEmpty);
         },
       );
     },
